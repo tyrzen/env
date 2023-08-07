d2dc0f1d3ee11afe8ae1116bfa04d5dd98533ad4
delveper <yevhen@bilyk.dev>
2023-08-07 10:25:38 +0300

    feat: refresh

--- env.go
+++ env.go
@@ -1,97 +1,232 @@
+/*
+Package env provides functions to load environment variables from a .env file into the system's
+environment variables, and to parse them into a given struct.
+
+It supports several tags:
+`default`- provides the default variable value.
+`env` - provides variable name that allows overriding the default variable.
+
+If tags are not provided, field names in the struct are automatically transformed
+to the conventional SNAKE_CASE with parent struct prefix to match environment variable.
+In case, a variable or the given struct field is not found,
+and default value is not provided; an error is returned.
+*/
 package env
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
 	"reflect"
 	"strconv"
 	"strings"
 	"time"
+	"unicode"
 )
 
-func LoadVars(pth string) error {
-	if pth == "" {
-		pth = ".env"
+const (
+	tagEnv     = "env"
+	tagDefault = "default"
+)
+
+// ParseTo loads the environment variables from .env file paths
+// and fills the configuration struct with the values.
+func ParseTo(dst any, envPaths ...string) error {
+	if envPaths == nil {
+		envPaths = []string{".env"}
 	}
+
+	for i := range envPaths {
+		if err := Load(envPaths[i]); err != nil {
+			return err
+		}
+	}
+
+	if err := parseTo(dst, ""); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Load loads the environment variables from a .env file into the system's.
+func Load(pth string) error {
 	env, err := os.Open(pth)
-	if err != nil {
-		return fmt.Errorf("opening environment file: %w", err)
+	if err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return fmt.Errorf("opening dotenv file: %w", err)
 	}
 
 	defer func() {
 		if err := env.Close(); err != nil {
-			log.Printf("closing environment file: %v", err)
+			log.Printf("closing dotenv file: %v", err)
 		}
 	}()
 
 	buf := bufio.NewScanner(env)
-	buf.Split(bufio.ScanLines)
-
 	for buf.Scan() {
-		if keyVal := strings.Split(buf.Text(), "="); len(keyVal) > 1 {
-			if err := os.Setenv(keyVal[0], keyVal[1]); err != nil {
-				return fmt.Errorf("setting environment variable: %w", err)
+		line := buf.Text()
+		if line == "" {
+			continue
+		}
+
+		key, val := parseLine(line)
+
+		if err := os.Setenv(key, val); err != nil {
+			return fmt.Errorf("setting %s[%s]: %w", key, val, err)
+		}
+	}
+
+	if err := buf.Err(); err != nil {
+		return fmt.Errorf("reading env file: %w", err)
+	}
+
+	return nil
+}
+
+// parseTo fills the struct fields
+// assigning the values from sources.
+func parseTo(dst any, prefix string) error {
+	v := reflect.ValueOf(dst).Elem()
+	t := v.Type()
+
+	for i := 0; i < t.NumField(); i++ {
+		fieldVal := v.Field(i)
+		fieldType := t.Field(i)
+
+		fieldName := prefix + fieldType.Name
+		if fieldType.Type.Kind() == reflect.Struct {
+			// recursive call is needed for nested structs.
+			if err := parseTo(fieldVal.Addr().Interface(), fieldName); err != nil {
+				return err
 			}
+
+			continue
+		}
+
+		val := getFieldValue(fieldType, fieldName)
+		if val == "" && prefix != "" {
+			return fmt.Errorf("no value for field: %s", fieldType.Name)
+		}
+
+		if err := setFieldValue(fieldType.Type, fieldVal, val); err != nil {
+			return err
 		}
 	}
 
 	return nil
 }
 
-func ParseVars[T any](pth, tag string) (*T, error) {
-	if tag == "" {
-		tag = "env"
+// getFieldValue gets the value for a field from different sources:
+// the environment variables or the `default` struct tag values.
+func getFieldValue(fieldType reflect.StructField, fieldName string) string {
+	envTag := fieldType.Tag.Get(tagEnv)
+	if val, ok := os.LookupEnv(envTag); ok {
+		return val
 	}
-	if err := LoadVars(pth); err != nil {
-		return nil, err
+
+	if val, ok := os.LookupEnv(camelToSnake(fieldName)); ok {
+		return val
 	}
 
-	dst := new(T)
-	val := reflect.ValueOf(dst).Elem()
+	val := fieldType.Tag.Get(tagDefault)
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Type().Field(i)
+	return val
+}
 
-		if field.Type.Kind() == reflect.Struct {
-			for j := 0; j < field.Type.NumField(); j++ {
-				envTag := field.Type.Field(j).Tag.Get(tag)
-				if envTag != "" {
-					envVal := os.Getenv(envTag)
-					if envVal == "" {
-						continue
-					}
+// parseLine parses a have from the .env file or value from os.Environ().
+func parseLine(line string) (string, string) {
+	i := strings.Index(line, "=")
+	if i <= 0 {
+		return "", ""
+	}
 
-					switch field.Type.Field(j).Type.Kind() {
-					case reflect.Int:
-						integer, err := strconv.Atoi(envVal)
-						if err != nil {
-							return nil, err
-						}
-						val.Field(i).Field(j).SetInt(int64(integer))
+	return line[:i], line[i+1:]
+}
 
-					case reflect.Float64:
-						float, err := strconv.ParseFloat(envVal, 64)
-						if err != nil {
-							return nil, err
-						}
-						val.Field(i).Field(j).SetFloat(float)
+// camelToSnake converts a CamelCase string to SNAKE_CASE.
+func camelToSnake(s string) string {
+	var (
+		parts []string
+		start int
+	)
 
-					case reflect.TypeOf(time.Duration(0)).Kind():
-						duration, err := time.ParseDuration(envVal)
-						if err != nil {
-							return nil, err
-						}
-						val.Field(i).Field(j).Set(reflect.ValueOf(duration))
-
-					default:
-						val.Field(i).Field(j).SetString(envVal)
-					}
-				}
-			}
+	for i, r := range s {
+		if unicode.IsUpper(r) && i != 0 {
+			parts = append(parts, s[start:i])
+			start = i
 		}
 	}
 
-	return dst, nil
+	parts = append(parts, s[start:])
+
+	for i, p := range parts {
+		parts[i] = strings.ToUpper(p)
+	}
+
+	return strings.Join(parts, "_")
+}
+
+// setFieldValue sets the value for a struct field according to a field type.
+//
+//nolint:cyclop
+func setFieldValue(fieldType reflect.Type, fieldVal reflect.Value, val string) error {
+	switch fieldType.Kind() {
+	case reflect.TypeOf(time.Duration(0)).Kind():
+		val, err := time.ParseDuration(val)
+		if err != nil {
+			return fmt.Errorf("parsing duration: %w", err)
+		}
+
+		fieldVal.Set(reflect.ValueOf(val))
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		val, err := strconv.ParseInt(val, 0, fieldType.Bits())
+		if err != nil {
+			return fmt.Errorf("parsing integer: %w", err)
+		}
+
+		fieldVal.SetInt(val)
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		val, err := strconv.ParseUint(val, 0, fieldType.Bits())
+		if err != nil {
+			return fmt.Errorf("parsing unsigned integer: %w", err)
+		}
+
+		fieldVal.SetUint(val)
+
+	case reflect.Float32, reflect.Float64:
+		val, err := strconv.ParseFloat(val, 64)
+		if err != nil {
+			return fmt.Errorf("parsing float: %w", err)
+		}
+
+		fieldVal.SetFloat(val)
+
+	case reflect.Bool:
+		val, err := strconv.ParseBool(val)
+		if err != nil {
+			return fmt.Errorf("parsing bool: %w", err)
+		}
+
+		fieldVal.SetBool(val)
+
+	case reflect.String:
+		fieldVal.SetString(val)
+
+	case reflect.Slice:
+		if fieldType.Elem().Kind() != reflect.String {
+			return fmt.Errorf("unsupported slice kind: %s", fieldType.Elem().Kind())
+		}
+
+		fieldVal.Set(reflect.ValueOf(strings.Split(val, ",")))
+
+	default:
+		return fmt.Errorf("unsupported field type: %s", fieldType.Name())
+	}
+
+	return nil
 }
--- env_test.go
+++ env_test.go
@@ -1,72 +1,255 @@
 package env
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"reflect"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
-// Config is a struct for testing ParseVars function.
-type Config struct {
-	Database struct {
-		Port     int           `env:"DB_PORT"`
-		Timeout  time.Duration `env:"DB_TIMEOUT"`
-		Password string        `env:"DB_PASSWORD"`
+func TestParseTo(t *testing.T) {
+	type config struct {
+		Home    string `env:"HOME"`
+		Default string `default:"default"`
+		Empty   int
+		Nested  struct{ Value string }
+	}
+
+	tests := map[string]struct {
+		vars    map[string]string
+		want    *config
+		wantErr error
+	}{
+		"All environment variables present": {
+			vars:    map[string]string{"HOME": "/home/test", "DEFAULT": "default", "EMPTY": "10", "NESTED_VALUE": "nested"},
+			want:    &config{Home: "/home/test", Default: "default", Empty: 10, Nested: struct{ Value string }{"nested"}},
+			wantErr: nil,
+		},
+		"No environment variables, except default present": {
+			vars:    nil,
+			want:    new(config),
+			wantErr: fmt.Errorf("no environment variables"),
+		},
+		"Some environment variables present": {
+			vars:    map[string]string{"HOME": "/home/test", "EMPTY": "10"},
+			want:    &config{Home: "/home/test", Empty: 10, Default: "default"},
+			wantErr: errors.New("no value for field: Value"),
+		},
+		"Environment variable set to empty string": {
+			vars:    map[string]string{"HOME": "", "DEFAULT": "", "EMPTY": "0", "NESTED_VALUE": ""},
+			want:    &config{Home: "", Default: "", Empty: 0},
+			wantErr: errors.New("no environment variables"),
+		},
+		"Environment variable set to invalid value": {
+			vars:    map[string]string{"HOME": "/home/test", "DEFAULT": "default", "EMPTY": "invalid"},
+			want:    nil,
+			wantErr: fmt.Errorf("parsing integer: strconv.ParseInt: parsing \"invalid\": invalid syntax"),
+		},
+		"Nested struct environment variable set": {
+			vars:    map[string]string{"HOME": "/home/test", "DEFAULT": "default", "EMPTY": "10", "NESTED_VALUE": "nested"},
+			want:    &config{Home: "/home/test", Default: "default", Empty: 10, Nested: struct{ Value string }{"nested"}},
+			wantErr: nil,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			teardown, err := setupEnv(t, tc.vars)
+			require.NoError(t, err)
+			defer teardown()
+
+			var cfg config
+			err = parseTo(&cfg, "")
+
+			if tc.wantErr != nil {
+				require.Error(t, err, tc.wantErr)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.want, &cfg)
+		})
 	}
 }
 
-// TestLoadVars tests LoadVars function.
-func TestLoadVars(t *testing.T) {
-	err := os.WriteFile("example.env", []byte("KEY=VALUE"), 0600)
-	if err != nil {
-		t.Fatalf("writing environment file: %v", err)
+func TestLoadEnv(t *testing.T) {
+	tests := map[string]struct {
+		vars    map[string]string
+		wantErr error
+	}{
+		"Valid environment file": {
+			vars:    map[string]string{"HOME": "/home/test", "DEFAULT": "default", "EMPTY": "10", "NESTED_VALUE": "nested"},
+			wantErr: nil,
+		},
+		"Empty variables": {
+			vars:    map[string]string{"": ""},
+			wantErr: errors.New("setting []: setenv: The parameter is incorrect"),
+		},
 	}
-	defer os.Remove("example.env")
 
-	err = LoadVars("example.env")
-	if err != nil {
-		t.Errorf("LoadVars() error = %v", err)
-		return
-	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			file, teardown, err := setupEnvFile(t, tc.vars)
+			require.NoError(t, err)
+			defer teardown()
 
-	got := os.Getenv("KEY")
-	want := "VALUE"
-	if got != want {
-		t.Errorf("os.Getenv() = %v, want %v", got, want)
+			teardown, err = setupEnv(t, tc.vars)
+			if tc.wantErr != nil {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			defer teardown()
+
+			err = Load(file.Name())
+			if tc.wantErr != nil {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			for k, v := range tc.vars {
+				require.Equal(t, v, os.Getenv(k))
+			}
+		})
 	}
 }
 
-// TestParseVars tests ParseVars function.
-func TestParseVars(t *testing.T) {
-	err := os.WriteFile("example.env", []byte("DB_PORT=3306\nDB_TIMEOUT=5s\nDB_PASSWORD=secret"), 0600)
-	if err != nil {
-		t.Fatalf("writing environment file: %v", err)
+func TestSetFieldValue(t *testing.T) {
+	tests := map[string]struct {
+		field   interface{}
+		have    string
+		want    interface{}
+		wantErr error
+	}{
+		"Duration":         {time.Duration(0), "1h", time.Hour, nil},
+		"Invalid Duration": {time.Duration(0), "invalid", nil, errors.New("parsing duration: time: invalid duration \"invalid\"")},
+		"Int":              {0, "123", 123, nil},
+		"Invalid Int":      {0, "invalid", nil, errors.New("parsing integer: strconv.ParseInt: parsing \"invalid\": invalid syntax")},
+		"Float":            {0.0, "1.23", 1.23, nil},
+		"Invalid Float":    {0.0, "invalid", nil, errors.New("parsing float: strconv.ParseFloat: parsing \"invalid\": invalid syntax")},
+		"Bool":             {false, "true", true, nil},
+		"Invalid Bool":     {false, "invalid", nil, errors.New("parsing bool: strconv.ParseBool: parsing \"invalid\": invalid syntax")},
+		"String":           {"", "test", "test", nil},
 	}
-	defer os.Remove("example.env")
 
-	got, err := ParseVars[Config]("example.env", "env")
-	if err != nil {
-		t.Errorf("ParseVars[Config]() error = %v", err)
-		return
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			fieldVal := reflect.New(reflect.TypeOf(tc.field)).Elem()
+			err := setFieldValue(reflect.TypeOf(tc.field), fieldVal, tc.have)
+
+			if tc.wantErr != nil {
+				require.Error(t, err, tc.wantErr)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.want, fieldVal.Interface())
+		})
+	}
+}
+
+func TestCamelToSnake(t *testing.T) {
+	tests := []struct {
+		name string
+		have string
+		want string
+	}{
+		{name: "Pascal case", have: "PascalCase", want: "PASCAL_CASE"},
+		{name: "Camel case", have: "camelCase", want: "CAMEL_CASE"},
+		{name: "Snake case", have: "snake_case", want: "SNAKE_CASE"},
+		{name: "Lowercase", have: "lowercase", want: "LOWERCASE"},
 	}
 
-	want := &Config{
-		Database: struct {
-			Port     int           `env:"DB_PORT"`
-			Timeout  time.Duration `env:"DB_TIMEOUT"`
-			Password string        `env:"DB_PASSWORD"`
-		}(struct {
-			Port     int
-			Timeout  time.Duration
-			Password string
-		}{
-			Port:     3306,
-			Timeout:  5 * time.Second,
-			Password: "secret",
-		}),
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := camelToSnake(tc.have)
+			require.Equal(t, tc.want, got)
+		})
 	}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("ParseVars[Config]() = %v, want %v", got, want)
+}
+
+func TestParseLine(t *testing.T) {
+	tests := map[string]struct {
+		have      string
+		wantKey   string
+		wantValue string
+	}{
+		"Line with key-value pair":         {have: "KEY=value", wantKey: "KEY", wantValue: "value"},
+		"Line with empty key":              {have: "EMPTY_KEY=", wantKey: "EMPTY_KEY", wantValue: ""},
+		"Line with empty value":            {have: "=EMPTY_VALUE", wantKey: "", wantValue: ""},
+		"Line without equal sign":          {have: "NO_EQUAL_SIGN", wantKey: "", wantValue: ""},
+		"Another have with key-value pair": {have: "ANOTHER_CASE=another_value", wantKey: "ANOTHER_CASE", wantValue: "another_value"},
 	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			key, value := parseLine(tc.have)
+			require.Equal(t, tc.wantKey, key)
+			require.Equal(t, tc.wantValue, value)
+		})
+	}
+}
+
+func setupEnv(t *testing.T, vars map[string]string) (func(), error) {
+	t.Helper()
+
+	if vars == nil {
+		return func() {}, nil
+	}
+
+	for k, v := range vars {
+		if err := os.Setenv(k, v); err != nil {
+			return func() {}, fmt.Errorf("setupEnv: setting %s[%s]: %v", k, v, err)
+		}
+	}
+
+	teardown := func() {
+		for k := range vars {
+			if err := os.Unsetenv(k); err != nil {
+				t.Errorf("setEnv: unsetting %s: %v", k, err)
+			}
+		}
+	}
+
+	return teardown, nil
+}
+
+func setupEnvFile(t *testing.T, vars map[string]string) (*os.File, func(), error) {
+	t.Helper()
+
+	file, err := os.CreateTemp(os.TempDir(), "test.env")
+	if err != nil {
+		return nil, func() {}, err
+	}
+
+	if vars == nil {
+		return file, func() {}, nil
+	}
+
+	for key, value := range vars {
+		_, err := file.WriteString(fmt.Sprintf("%s=%s\n", key, value))
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	teardown := func() {
+		err := file.Close()
+		if err != nil {
+			t.Error("createEnvFile: close:", err)
+		}
+
+		err = os.RemoveAll(file.Name())
+		if err != nil {
+			t.Error("createEnvFile: teardown:", err)
+		}
+	}
+
+	return file, teardown, nil
 }
